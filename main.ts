d0d09e943d7fed00eeda7d06952fcb074aefd379
dpqb.web <dpqb.web@proton.me>
2026-05-02 21:56:28 +0800

    ISTG

--- main.ts
+++ main.ts
@@ -9,18 +9,18 @@ export const PATH_CACHE: string = resolve("cache");
 // NOTE TO SELF: never use Fedora again. can't even do good things. fuck it.
 // uncomment below if you didn't use it
 
-// export const app: Webview = new Webview();
+export const app: Webview = new Webview();
 
-// app.navigate(`data:text/html,${
-//   encodeURIComponent(renderSPA({
-//     name: "Sistem Pendukung Keputusan - Pemilihan Penyedia Layanan Cloud",
-//     description:
-//       "Aplikasi Sistem Pendukung Keputusan bertema Pemilihan Penyedia Layanan Cloud",
-//     author: "Muhammad Rizki Fauzan",
-//   }))
-// }`);
-// app.bind("select", db.select);
-// app.bind("insert", db.insert);
-// app.bind("update", db.update);
-// app.bind("drop", db.drop);
-// app.run();
+app.navigate(`data:text/html,${
+  encodeURIComponent(renderSPA({
+    name: "Sistem Pendukung Keputusan - Pemilihan Penyedia Layanan Cloud",
+    description:
+      "Aplikasi Sistem Pendukung Keputusan bertema Pemilihan Penyedia Layanan Cloud",
+    author: "Muhammad Rizki Fauzan",
+  }))
+}`);
+app.bind("select", db.select);
+app.bind("insert", db.insert);
+app.bind("update", db.update);
+app.bind("drop", db.drop);
+app.run();
