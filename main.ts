de974c36cc02fdcfa115956d55618ca98ccedf36
dpqb.web <dpqb.web@proton.me>
2026-05-04 09:57:33 +0800

    simplify

--- main.ts
+++ main.ts
@@ -1,29 +1,13 @@
 import { Webview } from "jsr:@webview/webview";
 
-import { fetchBase64, renderSPA } from "./template.ts";
+import { SPA } from "./server.ts";
 import * as db from "./db.json.ts";
-
-const fonts: string[] = [
-  await fetchBase64("http://rsms.me/inter/font-files/InterVariable.woff2"),
-  await fetchBase64(
-    "http://rsms.me/inter/font-files/InterVariable-Italic.woff2",
-  ),
-];
 
 // NOTE TO SELF: never use Fedora again. can't even do good things. fuck it.
 
 export const app: Webview = new Webview();
 
-app.navigate(`data:text/html,${
-  encodeURIComponent(renderSPA({
-    names: [
-      "Sistem Pendukung Keputusan",
-      "Pemilihan Penyedia Layanan Cloud",
-    ],
-    author: "Muhammad Rizki Fauzan",
-    fonts,
-  }))
-}`);
+app.navigate(`data:text/html,${encodeURIComponent(SPA)}`);
 app.bind("select", db.select);
 app.bind("insert", db.insert);
 app.bind("update", db.update);
--- server.ts
+++ server.ts
@@ -6,21 +6,19 @@ const fonts: string[] = [
     "http://rsms.me/inter/font-files/InterVariable-Italic.woff2",
   ),
 ];
+export const SPA: string = renderSPA({
+  names: [
+    "Sistem Pendukung Keputusan",
+    "Pemilihan Penyedia Layanan Cloud",
+  ],
+  author: "Muhammad Rizki Fauzan",
+  fonts,
+});
 
 export default {
   fetch(_req: Request): Response {
-    return new Response(
-      renderSPA({
-        names: [
-          "Sistem Pendukung Keputusan",
-          "Pemilihan Penyedia Layanan Cloud",
-        ],
-        author: "Muhammad Rizki Fauzan",
-        fonts,
-      }),
-      {
-        headers: { "content-type": "text/html" },
-      },
-    );
+    return new Response(SPA, {
+      headers: { "content-type": "text/html" },
+    });
   },
 } satisfies Deno.ServeDefaultExport;
